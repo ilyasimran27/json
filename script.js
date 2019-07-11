706717734663efd84a8da3f42ae7ed9c7ac84cb0
Muhammad Imran <50030345+ilyasimran27@users.noreply.github.com>
2019-07-11 15:28:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+let contacts=[
+contact1={
+    name:"USman",
+    phone:["0343-0304456","0356-64768729"],
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    time:"1:00 am",
+    isRead:"ture"
+
+
+},
+{
+    name:"Imran",
+    phone:["0343-0304650","0356-64789729"],
+    time:"2:00 am",
+    profile_pic:"https://cdn.pixabay.com/photo/2015/12/01/20/28/fall-1072821__340.jpg",
+    isRead:"ture"
+
+},
+{
+    name:"Rizwan",
+    phone:["0343-0304650","0356-64789729"],
+    time:"7:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"false"
+
+},
+{
+    name:"kanwal",
+    phone:["0343-0304650","0356-64789729"],
+    time:"6:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"ture"
+
+},
+{
+    name:"Abdullah",
+    phone:["0343-0304650","0356-64789729"],
+    time:"2:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"false"
+
+},
+{
+    name:"hammad uni",
+    phone:["0343-0304650","0356-64789729"],
+    time:"8:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"ture"
+
+},
+{
+    name:"kishwar",
+    phone:["0343-0304650","0356-64789729"],
+    time:"9:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"ture"
+
+},
+{
+    name:"Uzma",
+    phone:["0343-0304650","0356-64789729"],
+    time:"8:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"ture"
+
+},
+{
+    name:"Ammar",
+    phone:["0343-0304650","0356-64789729"],
+    time:"2:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"ture"
+
+},
+{
+    name:"Nida",
+    phone:["0343-0304650","0356-64789729"],
+    time:"3:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"false"
+
+},
+{
+    name:"Jamal",
+    phone:["0343-0304650","0356-64789729"],
+    time:"8:00 am",
+    profile_pic:"https://images.pexels.com/photos/355296/pexels-photo-355296.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    isRead:"true"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
